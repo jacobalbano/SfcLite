569cd4acc96f7292cf3bde16442fae131fd2e17a
Jacob Albano <jacob.albano@gmail.com>
2024-01-18 20:51:13 -0500

    Optional globals, add script tag automatically

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export default function sfcLite(source) {
+export default function sfcLite(source, globals = {}) {
 	const parser = document.createElement('template');
 	parser.innerHTML = source;
 
@@ -7,30 +7,34 @@ export default function sfcLite(source) {
 	if (template == null) throw 'Failed to find template';
 	const script = content.querySelector('script');
 	const style = content.querySelector('style');
-	
-	// eslint-disable-next-line no-new-func
-	const setup = !script ? dummy : new Function('data', script.innerText);
-	const factoryId = `sfcLite_${++styleId}_${hashCode(source)}`;
+
+	const globalKeys = Object.keys(globals);
+	const globalValues = globalKeys.map(x => globals[x]);
+	const setup = makeFunction(script, globalKeys);
+
+	if (style != null) {
+		const styleEl = style.cloneNode(true);
+		styleEl.id = `sfcLite_${++styleId}_${hashCode(source)}`;
+		document.head.appendChild(styleEl);
+	}
 
 	return function(data) {
 		const fragment = template.content.cloneNode(true);
-		setup.call(fragment, data);
-
-		return {
-			fragment,
-			addStyle,
-		};
+		const params = [data, ...globalValues];
+		setup.call(fragment, ...params);
+		return fragment;
 	};
-	
-	function addStyle() {
-		if (style != null && !document.head.querySelector(`#${factoryId}`)) {
-			const styleEl = style.cloneNode(true);
-			styleEl.id = factoryId;
-			document.head.appendChild(styleEl);
-		}
-	}
 }
 
+function makeFunction(script, globalKeys) {
+	if (!script) return dummy;
+
+	const paramNames = ['data', ...globalKeys];
+	return new (script.async ? AsyncFunction : Function)(...paramNames, script.innerText);
+}
+
+// eslint-disable-next-line no-empty-function
+const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
 const dummy = () => null;
 const hashCode = (str) => [...str].reduce((s, c) => Math.imul(31, s) + c.charCodeAt(0) | 0, 0);
 let styleId = 0;
